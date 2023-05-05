518a032f0695eaae4415f7fe137bfce2776d0ec0
XxSkywalker <xiomarabarreto69@gmail.com>
2023-05-05 15:57:28 -0300

    deployment

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+
+const Footer = () => {
+    return (
+      <footer className="py-10">
+        <p className="text-center font-bold">APV - Administrador de Pacientes de {""}
+            <span className="text-rose-400">Veterinaria</span>
+        </p>
+      </footer>
+    )
+  }
+  
+  export default Footer
---
+++ src/components/ListadoPacientes.jsx
@@ -0,0 +1,37 @@
+import usePacientes from "../hooks/usePacientes"
+import Paciente from "./Paciente"
+
+const ListadoPacientes = () => {
+  const {pacientes} = usePacientes()
+  return (
+    <>
+      {pacientes.length ? (
+        <>
+          <h2 className="font-black text-3xl text-center">Listado de Pacientes</h2>
+
+          <p className=" text-xl mt-5 mb-10 text-center">
+            Administra tus <span className="text-rose-400 font-bold">Pacientes</span> y <span className="text-rose-400 font-bold">Citas</span>
+          </p>
+
+          {pacientes.map(paciente => (
+            <Paciente 
+                key={paciente._id}
+                paciente={paciente}
+            />
+          ))}
+        </>
+      ) : (
+        <>
+          <h2 className="font-black text-3xl text-center">No hay Pacientes</h2>
+
+          <p className=" text-xl mt-5 mb-10 text-center">
+            Comienza agregando pacientes <span className="text-rose-400 font-bold">y apareceran en este lugar</span>
+          </p>
+
+        </>
+      )}
+    </>
+  )
+}
+
+export default ListadoPacientes
